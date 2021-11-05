7a09fc5feca39465f76b781a5ec366cff1c28c74
Angel Daniel Lopez Vazquez <50413286+angeldaniel7328@users.noreply.github.com>
2021-11-04 18:42:12 -0600

    Agregacion de los metodos ActualizarBarco y EliminarBarco en la clase DALBarco

--- DataAccess/DALBarco.cs
+++ DataAccess/DALBarco.cs
@@ -20,7 +20,7 @@ namespace DataAccess
                 parametros.Add(new Parametro("@IdOwner", SqlDbType.Int, barco.IdOwner));
                 parametros.Add(new Parametro("@UrlFoto", SqlDbType.VarChar, barco.UrlFoto));
                 int rows = Consulta.EjecutarSinConsulta("SP_InsertarBarco", parametros);
-                return (rows == 1);
+                return (rows != 0);
             }
             catch (Exception)
             {
@@ -90,6 +90,43 @@ namespace DataAccess
                 throw new ArgumentException("No se pudo consultar en la base de datos");
             }
             return barcos;
+        }
+
+        public static bool ActualizarBarco(VOBarco barco)
+        {
+            try
+            {
+                List<Parametro> parametros = new List<Parametro>();
+                parametros.Add(new Parametro("@IdBarco", SqlDbType.Int, barco.IdBarco));
+                parametros.Add(new Parametro("@Matricula", SqlDbType.VarChar, barco.Matricula));
+                parametros.Add(new Parametro("@NoAmarre", SqlDbType.VarChar, barco.NoAmarre));
+                parametros.Add(new Parametro("@Nombre", SqlDbType.VarChar, barco.Nombre));
+                parametros.Add(new Parametro("@Cuota", SqlDbType.Decimal, barco.Cuota));
+                parametros.Add(new Parametro("@IdOwner", SqlDbType.Int, barco.IdOwner));
+                parametros.Add(new Parametro("@UrlFoto", SqlDbType.VarChar, barco.UrlFoto));
+                parametros.Add(new Parametro("@Disponibilidad", SqlDbType.Bit, barco.Disponibilidad));
+                int rows = Consulta.EjecutarSinConsulta("SP_ActualizarBarco", parametros);
+                return (rows != 0);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("No se pudo actualizar en la base de datos");
+            }
+        }
+
+        public static bool EliminarBarco(int idBarco)
+        {
+            try
+            {
+                List<Parametro> parametros = new List<Parametro>();
+                parametros.Add(new Parametro("@IdBarco", SqlDbType.Int, idBarco));
+                int rows = Consulta.EjecutarSinConsulta("SP_EliminarBarco", parametros);
+                return (rows != 0);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("No se pudo actualizar en la base de datos");
+            }
         }
     }
 }
