12f571c489796c0ea62728f5e84e7533baaa22c1
Angel Daniel Lopez Vazquez <50413286+angeldaniel7328@users.noreply.github.com>
2021-11-05 16:50:45 -0600

    Agregacion de los metodos ConsultarSalidasPorEstado, ConsultarSalidasPorIdExtendida y ConsultarSalidasPorEstadoExtendida en la clase DALSalida

--- DataAccess/DALSalida.cs
+++ DataAccess/DALSalida.cs
@@ -15,7 +15,6 @@ namespace DataAccess
             try
             {
                 List<Parametro> parametros = new List<Parametro>();
-
                 parametros.Add(new Parametro("@FechaHoraSalida", SqlDbType.DateTime, salida.FechaHoraSalida));
                 parametros.Add(new Parametro("@Destino", SqlDbType.VarChar, salida.Destino));
                 parametros.Add(new Parametro("@Estado", SqlDbType.VarChar, salida.Estado));
@@ -50,6 +49,67 @@ namespace DataAccess
                 throw new ArgumentException("No se pudo consultar en la base de datos");
             }
             return salida;
+        }
+
+        public static List<VOSalida> ConsultarSalidasPorEstado(string estado)
+        {
+            List<VOSalida> salidas = new List<VOSalida>();
+            try
+            {
+                List<Parametro> parametros = new List<Parametro>();
+                parametros.Add(new Parametro("@Estado", SqlDbType.VarChar, estado));
+                DataTable datosSalidas = Consulta.EjecutarConLlenado("SP_ConsultarSalidasPorEstado", parametros);
+                foreach (DataRow registro in datosSalidas.Rows) salidas.Add(new VOSalida(registro));
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("No se pudo consultar en la base de datos");
+            }
+            return salidas;
+        }
+
+        public static VOSalidaExtendida ConsultarSalidasPorIdExtendida(int idSalida)
+        {
+            VOSalidaExtendida salida;
+            try
+            {
+                List<Parametro> parametros = new List<Parametro>();
+                parametros.Add(new Parametro("@IdSalida", SqlDbType.Int, idSalida));
+                Dictionary<string, object> datos = Consulta.EjecutarLectura("SP_ConsultarSalidasPorIdExtendida", parametros);
+                DateTime fechaHoraSalida = (DateTime)datos["FechaHoraSalida"];
+                string destino = (string)datos["Destino"];
+                string estado = (string)datos["Estado"];
+                int idBarco = (int)datos["IdBarco"];
+                int idPersona = (int)datos["IdPersona"];
+                string nombreBarco = (string)datos["NombreBarco"];
+                string urlFotoBarco = (string)datos["UrlFotoBarco"];
+                string nombreCapitan = (string)datos["NombreCapitan"];
+                string urlFotoCapitan = (string)datos["UrlFotoCapitan"];
+                salida = new VOSalidaExtendida(idSalida, fechaHoraSalida, destino, estado, idBarco, idPersona,
+                    nombreBarco, urlFotoBarco, nombreCapitan, urlFotoCapitan);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("No se pudo consultar en la base de datos");
+            }
+            return salida;
+        }
+
+        public static List<VOSalidaExtendida> ConsultarSalidasPorEstadoExtendida(string estado)
+        {
+            List<VOSalidaExtendida> salidas = new List<VOSalidaExtendida>();
+            try
+            {
+                List<Parametro> parametros = new List<Parametro>();
+                parametros.Add(new Parametro("@Estado", SqlDbType.VarChar, estado));
+                DataTable datosSalidas = Consulta.EjecutarConLlenado("SP_ConsultarSalidasPorEstadoExtendida", parametros);
+                foreach (DataRow registro in datosSalidas.Rows) salidas.Add(new VOSalidaExtendida(registro));
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("No se pudo consultar en la base de datos");
+            }
+            return salidas;
         }
     }
 }
