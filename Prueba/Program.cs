af48f59f55c9ba6b35ce29d8a8844a04d792fdda
Angel Daniel Lopez Vazquez <50413286+angeldaniel7328@users.noreply.github.com>
2021-11-05 16:05:45 -0600

    Agregación de un proyecto de conssola para pruebas

---
+++ Prueba/Program.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess;
+using Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VOPersona persona = new VOPersona("9212856444", "Col. Miguel Hidalgo", 
+                "Angel Daniel", "adaniellova@gmail.com", ((int)Cargos.CAPITAN), true, 
+                "https://photo.isu.pub/angeldaniel321/photo_large.jpg");
+
+            DALPersona.InsertarPersona(persona);
+
+            List<VOBarco> barcos = DALBarco.ConsultarBarcos(null);
+            barcos.ForEach((VOBarco barco) => Console.WriteLine(barco.IdBarco));
+            Console.ReadKey();
+        }
+    }
+}
