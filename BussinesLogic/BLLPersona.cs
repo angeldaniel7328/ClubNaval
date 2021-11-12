c47e93cf91b7c50a9895faf6935e7fb7936c2448
Angel Daniel Lopez Vazquez <50413286+angeldaniel7328@users.noreply.github.com>
2021-11-12 16:57:31 -0600

    Agregacion de las clases en la capa de negocios para personas y salidas

---
+++ BussinesLogic/BLLPersona.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+using DataAccess;
+
+namespace BussinesLogic
+{
+    public static class BLLPersona
+    {
+        public static bool InsertarPersonna(VOPersona persona)
+        {
+            try
+            {
+                return DALPersona.InsertarPersona(persona);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Ocurrio un error " + e.Message);
+            }
+        }
+
+        public static bool ActualizarPersona(VOPersona persona)
+        {
+            try
+            {
+                return DALPersona.ActualizarPersona(persona);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Ocurrio un error " + e.Message);
+            }
+        }
+
+        public static bool EliminarPersona(string idPersona)
+        {
+            try
+            {
+                return DALPersona.EliminarPersona(int.Parse(idPersona));
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Ocurrio un error " + e.Message);
+            }
+        }
+
+        public static VOPersona ConsultarPersonaPorId(string idPersona)
+        {
+            VOPersona persona;
+            try
+            {
+                persona = DALPersona.ConsultarPersonaPorId(int.Parse(idPersona));
+                if (persona.Equals(null))
+                    throw new ArgumentException("El id buscado no existe en la base de datos");
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Ocurrio un error " + e.Message);
+            }
+            return persona;
+        }
+
+        public static List<VOPersona> ConsultarPersonas(bool? disponibilidad)
+        {
+            List<VOPersona> personas;
+            try
+            {
+                personas = DALPersona.ConsultarPersonas(disponibilidad);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Ocurrio un error " + e.Message);
+            }
+            return personas;
+        }
+
+        public static List<VOPersona> ConsultarPersonasPorCargo(int cargo, bool? disponibilidad)
+        {
+            List<VOPersona> personas;
+            try
+            {
+                personas = DALPersona.ConsultarPersonasPorCargo(cargo, disponibilidad);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Ocurrio un error " + e.Message);
+            }
+            return personas;
+        }
+    }
+}
